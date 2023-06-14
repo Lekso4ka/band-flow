ea724c3be154b711b29d1cea6e91cd9366423207
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-06-14 19:36:44 +0300

    Создали компонент Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Img from "../assets/img.png"
+
+const Card = () => {
+	return (
+		<div className="Card">
+			<Img />
+			<h2></h2>
+		</div>
+
+	)
+}
+
+export default Card
